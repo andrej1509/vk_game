65a5fd0c1ee28170b9a6c7ec99795593d71ef14d
Andrey Lopatin <kanzler1509@gmail.com>
2016-05-04 12:07:04 +0300

    end

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+var gameEngine;
+
 var x = 10,
     y = 10;
 function drawRect() {
@@ -9,8 +11,45 @@ function drawRect() {
   ctx.clearRect(0, 0, 300, 300);
   ctx.fillRect(x, y, 50, 50);
 }
+var nextGameStep = (function(){
+  return requestAnimationFrame ||
+  webkitRequestAnimationFrame ||
+  mozRequestAnimationFrame ||
+  oRequestAnimationFrame ||
+  msRequestAnimationFrame ||  
+  function (callback) {
+    setTimeout(callback, 1000/60);
+  };
+})();
 
-var z = setInterval(function(){
+var gameEngineStart = function (callback) {
+  gameEngine = callback;
+  gameEngineStep();
+}
+
+var gameEngineStep = function(){
+    gameEngine();
+    nextGameStep(gameEngineStep);
+}
+
+var setGameEngine = function(callback){
+  gameEngine = callback;
+}
+
+var gameLoopRight = function () {
   drawRect();
-  x+=52;
-},1000)
+  x+=1;
+  if(x>=100){
+    setGameEngine(gameLoopLeft);
+  }
+}
+
+var gameLoopLeft = function () {
+  drawRect();
+  x-=1;
+  if(x<=50){
+    setGameEngine(gameLoopRight);
+  }
+}
+
+gameEngineStart(gameLoopRight);
